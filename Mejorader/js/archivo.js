a085f6260361b774368f035f61c6ad1c3675bc59
MarlonCabrera <88162254+MarlonEFE@users.noreply.github.com>
2021-12-05 19:02:48 -0500

    Add files via upload

---
+++ Mejorader/js/archivo.js
@@ -0,0 +1,11 @@
+var enviar = document.getElementById("enviar");
+var confirmacion = document.getElementById("confirma");
+
+enviar.onclick = function(){
+    alert("Su Correo ha Sido enviado Con exito");
+    window.location = "index.html";
+}
+
+confirmacion.onclick = function(){
+    alert("se ha registrado con Exito");
+}
